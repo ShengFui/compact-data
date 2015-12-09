2b38c4d4abf78f3d84af16a87435fbc87f532163
jberinger <J.Beringer@gmail.com>
2015-12-09 15:16:39 +0100

    add numberStrings

--- src/main/java/net/sprd/bitcompact/BitStorage.java
+++ src/main/java/net/sprd/bitcompact/BitStorage.java
@@ -1,5 +1,7 @@
 package net.sprd.bitcompact;
 
+import net.sprd.compact.schema.ValueSize;
+
 public class BitStorage {
     
     public static int getInt(byte[] data, int bitOffset, int bitCount) {
@@ -57,25 +59,106 @@ public class BitStorage {
                 currentByteValue = ((data[bytePos]<< (8-byteBitOffset)) & 255) >> (8-byteBitOffset);
                 addBitPos -= byteBitOffset;
             }
-//            
-//            
-//            
-//            
-//            //check for last byte
-//            if (bytePos == lastByteOffset && lastByteBitOffset!=7) {
-//                int upBitOffset = lastByteBitOffset;
-////                addBitPos -=(7 - lastByteBitOffset);  // not necessary because of lastByte
-//                if (bytePos == byteOffset && byteBitOffset>0) { //check first = last byte
-//                    upBitOffset -=byteBitOffset;
-//                }
-//                byteValue = byteValue & ((1<<upBitOffset) -1);
-//            }
+
             data[bytePos] = (byte)(currentByteValue | byteValue);
             value = value >> addBitPos;
             bytePos++;
         }
         
     }
+    
+    public static long getLong(byte[] data, int bitOffset, int bitCount) {
+        int byteOffset = bitOffset>>3;
+        int byteBitOffset = bitOffset&7;
+        
+        int lastBit = bitOffset+bitCount-1;
+        int lastByteOffset = lastBit>>3;
+        int lastByteBitOffset = lastBit&7;
+        
+        int bytePos = byteOffset; //current byte position
+        int bitPos = 0; //current bit count of all used bytes
+        long value=0;//result value
+        while(bytePos<=lastByteOffset) {
+            long byteValue = data[bytePos] & 255;
+            int addBitPos = 8; //bitcount of current byte
+            //check for first byte
+            if (bytePos == byteOffset && byteBitOffset>0) {
+                byteValue = byteValue >> byteBitOffset;
+                addBitPos -= byteBitOffset;
+            }
+            //check for last byte
+            if (bytePos == lastByteOffset && lastByteBitOffset!=7) {
+                int upBitOffset = lastByteBitOffset;
+//                addBitPos -=(7 - lastByteBitOffset);  // not necessary because of lastByte
+                if (bytePos == byteOffset && byteBitOffset>0) { //check first = last byte
+                    upBitOffset -=byteBitOffset;
+                }
+                byteValue = ((byteValue<<(7-upBitOffset)) & 255) >>(7-upBitOffset);
+            }
+            value = value | (byteValue <<bitPos);
+            bitPos += addBitPos;
+            bytePos++;
+        }
+        return value;
+    }
+    
+    
+    public static void setLong(byte[] data, int bitOffset, int bitCount, long value) {
+        int byteOffset = bitOffset>>3;
+        int byteBitOffset = bitOffset&7;
+        
+        int lastBit = bitOffset+bitCount-1;
+        int lastByteOffset = lastBit>>3;
+        int lastByteBitOffset = lastBit&7;
+        
+        int bytePos = byteOffset; //current byte position
+        while(bytePos<=lastByteOffset) {
+            int currentByteValue = 0;
+            int byteValue = (int) (value & 255);
+            int addBitPos = 8; //bitcount of current byte
+            //check for first byte
+            if (bytePos == byteOffset && byteBitOffset>0) {
+                byteValue = (byteValue << byteBitOffset) & 255;
+                currentByteValue = ((data[bytePos]<< (8-byteBitOffset)) & 255) >> (8-byteBitOffset);
+                addBitPos -= byteBitOffset;
+            }
+
+            data[bytePos] = (byte)(currentByteValue | byteValue);
+            value = value >> addBitPos;
+            bytePos++;
+        }
+        
+    }
+    
+    public static final int numberStringTermination = 15;
+    
+    public static int setNumberString(byte[] data, int bitOffset, String value) {
+        int i=0;
+        int offset = bitOffset;
+        for (i = 0;i<value.length();i++) {
+            int numberValue = ((value.charAt(i)-'0') & 15);
+            setInt(data, offset, 4, numberValue);
+            offset += 4;
+        }
+        setInt(data, offset, 4, numberStringTermination);
+        offset += 4;
+        return offset-bitOffset;
+    }
+    
+    public static ValueSize<String> getNumberString(byte[] data, int bitOffset) {
+        StringBuilder sb = new StringBuilder();
+        int offset = bitOffset;
+        int val =0;
+        do {
+            val = getInt(data, offset, 4);
+            offset += 4;
+            if (val<numberStringTermination) {
+                sb.append((char)('0'+(val&15)));
+            }
+        } while (val < 15);
+        return new ValueSize<String>(sb.toString(), offset-bitOffset);
+    }
+    
     
     public static void print(byte[] data) {
         for(byte b:data) {
@@ -93,8 +176,8 @@ public class BitStorage {
         print01(data&2);
         print01(data&1);
         System.out.print(" ");
-        
     }
+    
     public static void print01(int data) {
         System.out.print((data==0?"0":"1"));
     }
--- src/test/java/net/sprd/bitcompact/BitStorageTest.java
+++ src/test/java/net/sprd/bitcompact/BitStorageTest.java
@@ -3,6 +3,8 @@ package net.sprd.bitcompact;
 import org.junit.Assert;
 import org.junit.Test;
 
+import net.sprd.compact.schema.ValueSize;
+
 public class BitStorageTest {
     
     @Test
@@ -20,12 +22,14 @@ public class BitStorageTest {
         testInt(0,0, 5, 32);
         testInt(0,1, 5, 32);
         testInt(0,-1, 5, 32);
+        System.out.println();
         
         testInt(-1,Integer.MAX_VALUE, 5, 32);
         testInt(-1,Integer.MIN_VALUE, 5, 32);
         testInt(-1,0, 5, 32);
         testInt(-1,1, 5, 32);
         testInt(-1,-1, 5, 32);
+        System.out.println();
     }
     
     public void testInt(int firstByte, int testInt, int bitOffset, int bitLength) {
@@ -35,6 +39,56 @@ public class BitStorageTest {
         BitStorage.print(data);
         int val = BitStorage.getInt(data, bitOffset, bitLength);
         Assert.assertEquals(testInt, val);
+    }
+    
+    @Test
+    public void testLong() {
+        
+        testLong(0,Long.MAX_VALUE, 0, 64);
+        testLong(0,Long.MIN_VALUE, 0, 64);
+        testLong(0,0, 0, 64);
+        testLong(0,1, 0, 64);
+        testLong(0,-1, 0, 64);
+        System.out.println();
+        
+        testLong(0,Long.MAX_VALUE, 5, 64);
+        testLong(0,Long.MIN_VALUE, 5, 64);
+        testLong(0,0, 5, 64);
+        testLong(0,1, 5, 64);
+        testLong(0,-1, 5, 64);
+        System.out.println();
+        
+        testLong(-1,Long.MAX_VALUE, 5, 64);
+        testLong(-1,Long.MIN_VALUE, 5, 64);
+        testLong(-1,0, 5, 64);
+        testLong(-1,1, 5, 64);
+        testLong(-1,-1, 5, 64);
+        System.out.println();
+    }
+    
+    public void testLong(int firstByte, long testLong, int bitOffset, int bitLength) {
+        byte[] data = new byte[10];
+        data[0] = (byte)firstByte;
+        BitStorage.setLong(data, bitOffset, bitLength, testLong);
+        BitStorage.print(data);
+        long val = BitStorage.getLong(data, bitOffset, bitLength);
+        Assert.assertEquals(testLong, val);
+    }
+    
+    @Test
+    public void testNumberString() {
+        testNumberString(-1, "12987", 0);
+        testNumberString(-1, "12987", 4);
+        testNumberString(-1, "12987", 5);
+    }
+    
+    public void testNumberString(int firstByte, String testValue, int bitOffset) {
+        byte[] data = new byte[10];
+        data[0] = (byte)firstByte;
+        BitStorage.setNumberString(data, bitOffset, testValue);
+        BitStorage.print(data);
+        ValueSize<String> value = BitStorage.getNumberString(data, bitOffset);
+        Assert.assertEquals(testValue, value.getValue());
     }
 
 }
