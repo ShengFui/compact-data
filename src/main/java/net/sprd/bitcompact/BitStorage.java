0fda10692be59ca40344261ba181e5b993672258
jberinger <jbe@spreadshirt.net>
2015-12-09 12:55:38 +0100

    prepare bitstorage

---
+++ src/main/java/net/sprd/bitcompact/BitStorage.java
@@ -0,0 +1,12 @@
+package net.sprd.bitcompact;
+
+public class BitStorage {
+    
+    public static int getInt(byte[] data, int bitOffset, int offset, int bitCount) {
+        return 0;
+    }
+    
+    public static void setInt(byte[] data, int bitOffset, int offset, int bitCount, int value) {
+    }
+
+}
