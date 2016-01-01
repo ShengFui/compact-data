3398bbce502961dc986b909b7cbf810b76130e18
jberinger <J.Beringer@gmail.com>
2016-01-01 11:32:51 +0100

    optimize floating

--- src/main/java/net/sprd/bitcompact/schema/FloatingBitSchema.java
+++ src/main/java/net/sprd/bitcompact/schema/FloatingBitSchema.java
@@ -6,7 +6,15 @@ public class FloatingBitSchema extends AbstractDoubleBitSchema{
 
     private double factor;
     private double minValue;
-    
+    /**
+     * store unsigned integer for (value-minValue)*factor with bitCount bits
+     * handle values between [minValue ; minValue + ((2^bitCount -1)/factor)], 
+     * precision (minimal distance between 2 values) is 1/factor 
+     * 
+     * @param bitCount number of bits to store
+     * @param factor factor to scale number to int
+     * @param minValue minimum value
+     */
     public FloatingBitSchema(int bitCount, double factor, double minValue) {
         super(bitCount);
         this.factor=factor;
